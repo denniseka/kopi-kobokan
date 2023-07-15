8da7488f7c5de03d494d84a0ddbab2705a7cb280
Dennis Eka Saputra <denniseka04@gmail.com>
2023-07-15 18:53:25 +0700

    memperbaiki fitur searching

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 const navbarNav = document.querySelector('.navbar-nav');
 const humburgerMenu = document.querySelector('#humburger-menu');
 
+const btnSearch = document.getElementById('search');
+const searchForm = document.querySelector('.search-form');
+
 // klik humburger menu untuk menampilkan navbar
 humburgerMenu.onclick = () => {
   navbarNav.classList.toggle('active');
@@ -13,4 +16,13 @@ document.addEventListener('click', function (e) {
   if (!humburgerMenu.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove('active');
   }
+  if (!btnSearch.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove('active');
+  }
+});
+
+// Menampilkan tampilah search
+
+btnSearch.addEventListener('click', function (e) {
+  searchForm.classList.toggle('active');
 });
